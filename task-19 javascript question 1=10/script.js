3f4cf0685d6a98b17e2bb48b3062283bba2b48e3
imzaraahmed <imzarajamal@gmail.com>
2025-08-10 01:10:18 +0500

    javascript solutions of 4 questions

---
+++ task-19 javascript question 1=10/script.js
@@ -0,0 +1,37 @@
+// question-1 take two nos from user and display their sum
+
+function q1 () {
+var q1num1 = parseInt(prompt("Enter the first integer"));
+var q1num2 = parseInt(prompt("Enter the second integer"));
+var q1totalSum = q1num1 + q1num2;
+alert("The sum of two integers is " + q1totalSum);
+}
+
+// Write a program to add three numbers entered by the user.
+
+function q2 () {
+var q2num1 = parseInt(prompt("Enter the first integer"));
+var q2num2 = parseInt(prompt("Enter the second integer"));
+var q2num3 = parseInt(prompt("Enter the third integer"));
+var q2totalSum = q2num1 + q2num2 + q2num3;
+alert("The sum of threeintegers is " + q2totalSum);
+}
+
+//Ask the user for two numbers, add them, and show the result using alert().
+
+function q3 () {
+var q3num1 = parseInt(prompt("Enter the first integer"));
+var q3num2 = parseInt(prompt("Enter the second integer"));
+var q3totalSum = q3num1 + q3num2;
+alert("The sum of two integers is " + q3totalSum);
+}
+
+//Take two numbers and display their sum, difference, product, and quotient.
+function q4 () {
+var q3num1 = parseInt(prompt("Enter the first integer"));
+var q3num2 = parseInt(prompt("Enter the second integer"));
+var q3totalSum = q3num1 + q3num2;
+var q4difference = q3num1 - q3num2;
+var q4quotient = q3num1 / q3num2;
+console.log("The sum of two integers is " + q3totalSum + "\nThe difference of two integers is " + q4difference+ "\nThe quotient of two integers is " + q4quotient);
+}
